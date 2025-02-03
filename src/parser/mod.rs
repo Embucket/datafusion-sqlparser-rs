25f863e4233be4edaa1c806d2ef460767eb3e68c
DanCodedThis <94703934+DanCodedThis@users.noreply.github.com>
2025-02-03 17:19:08 +0200

    Add ShowObjects statement (#2)
    * Added ShowObjects statement
    
    * Parsing, next tests
    
    * Small comment
    
    * Tests done according to other SHOW statements
    
    * Removed unnecessary comments

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -14594,7 +14594,7 @@ impl<'a> Parser<'a> {
         false
     }
 
-    pub(crate) fn parse_show_stmt_options(&mut self) -> Result<ShowStatementOptions, ParserError> {
+    pub fn parse_show_stmt_options(&mut self) -> Result<ShowStatementOptions, ParserError> {
         let show_in;
         let mut filter_position = None;
         if self.dialect.supports_show_like_before_in() {
