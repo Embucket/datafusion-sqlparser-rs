d5561819f2e8f0415b6d5873f3a29e8de3301067
Denys Tsomenko <denistsem@gmail.com>
2025-02-03 22:15:09 +0200

    Revert "fix parsing of `INSERT INTO ... SELECT ... RETURNING ` (#1661)"
    This reverts commit 38ef2bf8eb36bd0e60a379f31f2f9589fe7c692c.

--- src/keywords.rs
+++ src/keywords.rs
@@ -954,7 +954,6 @@ pub const RESERVED_FOR_TABLE_ALIAS: &[Keyword] = &[
     Keyword::GLOBAL,
     Keyword::ANTI,
     Keyword::SEMI,
-    Keyword::RETURNING,
     // for MSSQL-specific OUTER APPLY (seems reserved in most dialects)
     Keyword::OUTER,
     Keyword::SET,
